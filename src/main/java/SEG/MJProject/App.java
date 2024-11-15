aacaa9689d851747a5407ab97e8098ba95aa5acd
Gayatri <gayatridevi@gmail.com>
2024-11-15 09:51:01 +0530

    First commit to maven java project

---
+++ src/main/java/SEG/MJProject/App.java
@@ -0,0 +1,13 @@
+package SEG.MJProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! -22BD1A664R -Haniya Fatima" );
+    }
+}
